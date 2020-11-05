5e9bce1130c4c20ba319d5ae83b1e18c1d243879
Lander <59607028+lp150202@users.noreply.github.com>
2020-11-05 22:50:58 +0100

    Added language class

---
+++ src/domain/Languages.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Languages {
+    String language;
+    String level;
+
+    public Languages(String language, String level) {
+        this.language = language;
+        this.level = level;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
