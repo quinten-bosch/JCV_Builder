2196396cd056cfa116c86d7c83b9a8fe5a5056c1
Lander <59607028+lp150202@users.noreply.github.com>
2020-11-05 12:24:33 +0100

    updated personal info getter and setter

--- src/domain/PersonalInfo.java
+++ src/domain/PersonalInfo.java
@@ -2,9 +2,220 @@ package domain;
 
 public class PersonalInfo {
 
-    private String firstName, lastName, firstNameValue, lastNameValue;
-    private String city, street, streetNr, zip, cityValue, streetValue, streetNrValue, zipValue;
-    private String age, birthDate, ageValue, birthDateValue;
-    private String phoneNumber, email, phoneNumberValue, emailValue;
+    String firstName, lastName, firstNameValue, lastNameValue;
+    String city, street, streetNr, zip, cityValue, streetValue, streetNrValue, zipValue, cityActive, streetActive,streetNrActive, zipActive;
+    String age, birthDate, ageValue, birthDateValue, ageActive, birthDateActive;
+    String phoneNumber, email, phoneNumberValue, emailValue;
 
+    public PersonalInfo(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstNameValue() {
+        return firstNameValue;
+    }
+
+    public void setFirstNameValue(String firstNameValue) {
+        this.firstNameValue = firstNameValue;
+    }
+
+    public String getLastNameValue() {
+        return lastNameValue;
+    }
+
+    public void setLastNameValue(String lastNameValue) {
+        this.lastNameValue = lastNameValue;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNr() {
+        return streetNr;
+    }
+
+    public void setStreetNr(String streetNr) {
+        this.streetNr = streetNr;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCityValue() {
+        return cityValue;
+    }
+
+    public void setCityValue(String cityValue) {
+        this.cityValue = cityValue;
+    }
+
+    public String getStreetValue() {
+        return streetValue;
+    }
+
+    public void setStreetValue(String streetValue) {
+        this.streetValue = streetValue;
+    }
+
+    public String getStreetNrValue() {
+        return streetNrValue;
+    }
+
+    public void setStreetNrValue(String streetNrValue) {
+        this.streetNrValue = streetNrValue;
+    }
+
+    public String getZipValue() {
+        return zipValue;
+    }
+
+    public void setZipValue(String zipValue) {
+        this.zipValue = zipValue;
+    }
+
+    public String getCityActive() {
+        return cityActive;
+    }
+
+    public void setCityActive(String cityActive) {
+        this.cityActive = cityActive;
+    }
+
+    public String getStreetActive() {
+        return streetActive;
+    }
+
+    public void setStreetActive(String streetActive) {
+        this.streetActive = streetActive;
+    }
+
+    public String getStreetNrActive() {
+        return streetNrActive;
+    }
+
+    public void setStreetNrActive(String streetNrActive) {
+        this.streetNrActive = streetNrActive;
+    }
+
+    public String getZipActive() {
+        return zipActive;
+    }
+
+    public void setZipActive(String zipActive) {
+        this.zipActive = zipActive;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAgeValue() {
+        return ageValue;
+    }
+
+    public void setAgeValue(String ageValue) {
+        this.ageValue = ageValue;
+    }
+
+    public String getBirthDateValue() {
+        return birthDateValue;
+    }
+
+    public void setBirthDateValue(String birthDateValue) {
+        this.birthDateValue = birthDateValue;
+    }
+
+    public String getAgeActive() {
+        return ageActive;
+    }
+
+    public void setAgeActive(String ageActive) {
+        this.ageActive = ageActive;
+    }
+
+    public String getBirthDateActive() {
+        return birthDateActive;
+    }
+
+    public void setBirthDateActive(String birthDateActive) {
+        this.birthDateActive = birthDateActive;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumberValue() {
+        return phoneNumberValue;
+    }
+
+    public void setPhoneNumberValue(String phoneNumberValue) {
+        this.phoneNumberValue = phoneNumberValue;
+    }
+
+    public String getEmailValue() {
+        return emailValue;
+    }
+
+    public void setEmailValue(String emailValue) {
+        this.emailValue = emailValue;
+    }
 }
--- src/ui/CvApp.java
+++ src/ui/CvApp.java
@@ -1,7 +1,9 @@
 package ui;
 
+import domain.*;
+
 public class CvApp {
     public static void main(String[] args) {
-
+        PersonalInfo.getFirstName();
     }
 }
