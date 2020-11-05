e01d0461ad60c2b7b3b2a41872a57508ee41613e
Lander <59607028+lp150202@users.noreply.github.com>
2020-11-05 12:07:42 +0100

    Created personal info class

---
+++ src/domain/PersonalInfo.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class PersonalInfo {
+
+    private String firstName, lastName, firstNameValue, lastNameValue;
+    private String city, street, streetNr, zip, cityValue, streetValue, streetNrValue, zipValue;
+    private String age, birthDate, ageValue, birthDateValue;
+    private String phoneNumber, email, phoneNumberValue, emailValue;
+
+}
