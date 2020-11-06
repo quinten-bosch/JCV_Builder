5a064fdfb43b6340a69d661b840a8ccf7a5abb13
Lander <59607028+lp150202@users.noreply.github.com>
2020-11-06 21:12:40 +0100

    Added education block

---
+++ src/domain/Education.java
@@ -0,0 +1,157 @@
+package domain;
+
+public class Education {
+    String education, educationValue;
+    String place, placeValue;
+    String educationalInstitution, educationalInstitutionValue;
+    String fromMonth, fromMonthValue, fromYear, fromYearValue;
+    String untilMonth, untilMonthValue, untilYear, untilYearValue;
+    String description, descriptionValue;
+
+    public Education(String education, String educationValue, String place, String placeValue, String educationalInstitution, String educationalInstitutionValue, String fromMonth, String fromMonthValue, String fromYear, String fromYearValue, String untilMonth, String untilMonthValue, String untilYear, String untilYearValue, String description, String descriptionValue) {
+        this.education = education;
+        this.educationValue = educationValue;
+        this.place = place;
+        this.placeValue = placeValue;
+        this.educationalInstitution = educationalInstitution;
+        this.educationalInstitutionValue = educationalInstitutionValue;
+        this.fromMonth = fromMonth;
+        this.fromMonthValue = fromMonthValue;
+        this.fromYear = fromYear;
+        this.fromYearValue = fromYearValue;
+        this.untilMonth = untilMonth;
+        this.untilMonthValue = untilMonthValue;
+        this.untilYear = untilYear;
+        this.untilYearValue = untilYearValue;
+        this.description = description;
+        this.descriptionValue = descriptionValue;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getEducationValue() {
+        return educationValue;
+    }
+
+    public void setEducationValue(String educationValue) {
+        this.educationValue = educationValue;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPlaceValue() {
+        return placeValue;
+    }
+
+    public void setPlaceValue(String placeValue) {
+        this.placeValue = placeValue;
+    }
+
+    public String getEducationalInstitution() {
+        return educationalInstitution;
+    }
+
+    public void setEducationalInstitution(String educationalInstitution) {
+        this.educationalInstitution = educationalInstitution;
+    }
+
+    public String getEducationalInstitutionValue() {
+        return educationalInstitutionValue;
+    }
+
+    public void setEducationalInstitutionValue(String educationalInstitutionValue) {
+        this.educationalInstitutionValue = educationalInstitutionValue;
+    }
+
+    public String getFromMonth() {
+        return fromMonth;
+    }
+
+    public void setFromMonth(String fromMonth) {
+        this.fromMonth = fromMonth;
+    }
+
+    public String getFromMonthValue() {
+        return fromMonthValue;
+    }
+
+    public void setFromMonthValue(String fromMonthValue) {
+        this.fromMonthValue = fromMonthValue;
+    }
+
+    public String getFromYear() {
+        return fromYear;
+    }
+
+    public void setFromYear(String fromYear) {
+        this.fromYear = fromYear;
+    }
+
+    public String getFromYearValue() {
+        return fromYearValue;
+    }
+
+    public void setFromYearValue(String fromYearValue) {
+        this.fromYearValue = fromYearValue;
+    }
+
+    public String getUntilMonth() {
+        return untilMonth;
+    }
+
+    public void setUntilMonth(String untilMonth) {
+        this.untilMonth = untilMonth;
+    }
+
+    public String getUntilMonthValue() {
+        return untilMonthValue;
+    }
+
+    public void setUntilMonthValue(String untilMonthValue) {
+        this.untilMonthValue = untilMonthValue;
+    }
+
+    public String getUntilYear() {
+        return untilYear;
+    }
+
+    public void setUntilYear(String untilYear) {
+        this.untilYear = untilYear;
+    }
+
+    public String getUntilYearValue() {
+        return untilYearValue;
+    }
+
+    public void setUntilYearValue(String untilYearValue) {
+        this.untilYearValue = untilYearValue;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescriptionValue() {
+        return descriptionValue;
+    }
+
+    public void setDescriptionValue(String descriptionValue) {
+        this.descriptionValue = descriptionValue;
+    }
+}
