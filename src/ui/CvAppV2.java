2a1bd16d1bee6927a7acdbb9e50c8c4086e58891
Lander <59607028+lp150202@users.noreply.github.com>
2020-11-07 22:27:48 +0100

    Update CvAppV2.java

--- src/ui/CvAppV2.java
+++ src/ui/CvAppV2.java
@@ -7,19 +7,23 @@ public class CvAppV2 {
     public static void main(String[] args) {
         session_ID = args[0];
         int teller = 0;
+        int skillsTeller = 0;
 
         Template template1 = new Template("template1", 1);
         template1.createTemplateFile();
 
+        Skills[] skills = new Skills[3];
+
         for (String arg : args) {
             System.out.println(arg);
             if (arg.equals("Skills")) {
-                Skills skill2 = new Skills("hallo","goed");
-                template1.addSkills(skill2);
-                template1.writeSkillsToFile();
+                skills[skillsTeller] = new Skills("hallo","goed");
+                template1.addSkills(skills[skillsTeller]);
+                skillsTeller++;
             }
             teller++;
         }
+        template1.writeSkillsToFile();
         System.out.println("SessionID: "+session_ID);
     }
 }
